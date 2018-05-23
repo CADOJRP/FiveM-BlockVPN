dfafda6eb6728e7d8eaf6784a4ecbf8639f7c46e
Avery Johnson <avery627@gmail.com>
2018-05-23 12:24:45 -0400

    Removed Semicolons

--- blockVPN/server.lua
+++ blockVPN/server.lua
@@ -1,10 +1,10 @@
 --------------
 --  CONFIG  --
 --------------
-local ownerEmail = '';             -- Owner Email (Required) - No account needed (Used Incase of Issues)
-local kickThreshold = 0.99;        -- Anything equal to or higher than this value will be kicked. (0.99 Recommended as Lowest)
-local kickReason = 'We\'ve detected that you\'re using a VPN or Proxy. If you belive this is a mistake please contact the administration team.';
-local printFailed = true;
+local ownerEmail = ''             -- Owner Email (Required) - No account needed (Used Incase of Issues)
+local kickThreshold = 0.99        -- Anything equal to or higher than this value will be kicked. (0.99 Recommended as Lowest)
+local kickReason = 'We\'ve detected that you\'re using a VPN or Proxy. If you belive this is a mistake please contact the administration team.'
+local printFailed = true
 
 
 
